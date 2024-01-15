075f0d587dedfb60c5ffa01af79be8a987173693
apzakharov <apzakharov9881@gmail.com>
2024-01-16 02:49:14 +0300

    MoveRobot(new)

---
+++ CS_Course/src/main/java/org/example/java_base/module_4/4.2 - MoveRobot(new).java
@@ -0,0 +1,46 @@
+package org.example.java_base// 4.2 - реализовать метод который устанавливает соединение с роботом,
+// отдает ему команду на перемещение в заданную точку и затем закрывает соединение,
+// выполняя при необходимости повтор этой последовательности до трех раз.
+
+public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+  RobotConnection robot = null;
+        boolean needToClose = true;
+        for (int i=0; i<3; i++) {
+            try {
+                robot = robotConnectionManager.getConnection();
+                robot.moveRobotTo(toX, toY);
+                return;
+            }
+            catch (RobotConnectionException e) {
+                if(i==2){
+                    if (robot != null)
+                        robot.close();
+                    needToClose = false;
+                    throw e;
+                }
+            }
+            catch (Exception ex) {
+                try{
+                    if ((robot != null) && needToClose){
+                        robot.close();
+                        needToClose = false;
+                    }
+                }
+                catch(Exception exp){
+                    
+                }
+                throw ex;
+            }
+            finally {
+                try{
+                    if ((robot != null) && needToClose)
+                        robot.close();
+                }
+                catch(Exception exp){
+                   
+                }
+
+            }
+        }
+
+    }
